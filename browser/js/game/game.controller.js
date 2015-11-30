9f1cbf1bc83168d36e99e4082d4c1bdf817a6b6b
Luis Martins <luis.m.martins91@gmail.com>
2015-11-29 22:44:49 -0500

    Players vs NumCities Bar Graph

--- browser/js/game/game.controller.js
+++ browser/js/game/game.controller.js
@@ -1,4 +1,5 @@
 app.controller('GameCtrl', function($scope, FirebaseFactory, gridId, key){
     $scope.grid = FirebaseFactory.getConnection(key);
+    console.log('$scope.grid', $scope.grid)
     $scope.gridGame = FirebaseFactory.getConnection(key + '/game'); 
 }); 
